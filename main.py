65db63d4fe2158f6f1a25085fcf3c87e388a3f6c
Daniel <122654917+dtecoder46@users.noreply.github.com>
2023-02-14 13:11:11 +0000

    List options added.

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ slides=int(input("How many slides do you want?: "))
 print()
 
 for text_input in range(slides):
-  print("•••••••")
+  list_option=input("Do you want a numbered list, bullet list, none, or all?")
+  
+  print("<slide break>\n")
   text=input("Enter the slide text: ")
   title=input("What is the slide title?: ")
-  print("•••••••\n")
+  print("\n<slide break>\n")
   
   print("-------")
   print(title,"\n")
