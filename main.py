37dac4a650ded7d6f77799e44a23a13c63527c58
Daniel <122654917+dtecoder46@users.noreply.github.com>
2023-02-23 02:04:23 +0000

    Removed list options, cleaned up the oupturn.

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ slides=int(input("How many slides do you want?: "))
 print()
 
 for text_input in range(slides):
-  list_option=input("Do you want a numbered list, bullet list, none, or all?")
-  
-  print("<slide break>\n")
+  if text_input>0:
+    print("\n<slide break>\n")
   text=input("Enter the slide text: ")
-  title=input("What is the slide title?: ")
+  title=input("\nWhat is the slide title?: ")
   print("\n<slide break>\n")
   
   print("-------")
